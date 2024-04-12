635b64d667b52b7dc0dfebf21e338aa5fc050527
Xavier deSouza <103150835+xdesou01@users.noreply.github.com>
2024-04-12 04:17:49 -0400

    Fixed Bugs
    -subtacting in the wrong order (secondMin - jVal) 
    -pushing the inductive vector to the back of mat instead of the front (J needs to view the inductive "unsolved" part row first)
    
    Inductive solution now appears to work

--- Grigoriev.cpp
+++ Grigoriev.cpp
@@ -17,9 +17,16 @@ using namespace std;
 
 vector<vector<int>> retrieveMatrix() {
     //vector<vector<int>> x(4, vector<int>(5, 1)); //change for input
-
+    // vector<vector<int>> x = {{1, 0}}; //works
+    // vector<vector<int>> x = {{0, 1}}; //works
     //vector<vector<int>> x = {{1, 2, 3}, {3, 2, 1}}; //works, solution found
-    vector<vector<int>> x = {{0, 1, 2, 3}, {0, 0, 1, 3}, {1, 0, 0, 2}, {0, 1, 0, 2}}; //works, solution found
+    vector<vector<int>> x = {{0, 1, 2, 3}, 
+                             {0, 0, 1, 3}, 
+                             {1, 0, 0, 2}, 
+                             {0, 1, 0, 2}}; //works, solution found
+    //vector<vector<int>> x = {{1, 2, 3, 4}};
+    //vector<vector<int>> x = {{0, 1, 2, 3}, {0, 0, 1, 3}, {1, 0, 0, 2}, {0, 1, 0, 2}};
+    // vector<vector<int>> x = {{1, 1, 0, 3}, {1, 2, 1, 3}};
     //vector<vector<int>> x = {{1, 2}, {3, 2}}; //works, no solution detected
     return x;
 }
@@ -55,6 +62,9 @@ class Matrix {
     void swapRows(int r1, int r2) {
         std::swap(mat[r1 - 1], mat[r2 - 1]);
     }
+    void addRow(vector<int> &v) {
+        mat.insert(mat.begin(), v);
+    }
 };
 
 class SolnVector {
@@ -87,14 +97,137 @@ class SolnVector {
     //if |J| == 1, success (do some steps and return x)
     //if |J| == n, fail (no solution as demonstrated by [dis]provable by contradiction)
     //otherwise, increment values in x as appropriate
+SolnVector Grigoriev_Algorithm(vector<vector<int>> &matrix);
+void solve_Grigoriev(Matrix &matrix, SolnVector &x);
+
 
 int main(int argc, char *argv[]) {
-    vector<vector<int>> vec_matrix = retrieveMatrix(); //input
-    Matrix matrix(vec_matrix);
+    vector<vector<int>> input_matrix = retrieveMatrix(); //input
+    SolnVector x = Grigoriev_Algorithm(input_matrix);
+//     vector<vector<int>> vec_matrix = retrieveMatrix(); //input
+//     Matrix matrix(vec_matrix);
 
+//     int m = matrix.rows(), n = matrix.cols();
+//     SolnVector x(n); //solution vector initialized with zeroes
+
+//     while (true) {
+//         unordered_set<int> J;
+//         for (int i = 1; i <= m; i++) {
+//             //cout << "J SIZE2 = " << J.size() << endl;
+//             int curMin = INT_MAX;
+//             unordered_set<int> curMinColsNotInJ;
+//             for (int j = 1; j <= n; j++) {
+//                 int curVal = matrix.get(i, j) + x.get(j);
+//                 if (curVal < curMin) {
+//                     curMin = curVal;
+//                     curMinColsNotInJ.clear();
+//                     insertIfNotInJ(j, J, curMinColsNotInJ);
+
+//                 }
+//                 else if (curVal == curMin) {
+//                     insertIfNotInJ(j, J, curMinColsNotInJ);
+//                     //cout << curMinColsNotInJ.size() << ", " << J.size() << endl;
+//                 }
+//             }
+//             if (curMinColsNotInJ.size() == 1) {
+//                 for (int j : curMinColsNotInJ) {
+//                     //cout << "inserting: " << j << endl;
+//                     J.insert(j); //extend J with j, continue
+//                 }
+//             }
+//         }
+
+//         int k = J.size();
+//         if (k == n) {
+//             cout << "there is no solution for this input" << endl;
+//             return EXIT_SUCCESS;
+//         }
+//         if (k == 0) { //is this an end case?
+//             x.printSolution();
+//             return EXIT_SUCCESS;
+//         }
+//         if (k == 1) {
+
+// //ASK ABOUT THIS
+// // Now, let k = |J| < n. If k = 1, we add to x1 the number
+// // min2≤j≤n{a1,j +xj}−(a1,1 +x1) ≥ 1 and obtain a solution of (0.1).
+// // Thereupon, we apply Lemma 1.2 to the obtained solution; as a
+// // result, the algorithm terminates the inductive step
+
+
+// //J contains the only column that has a row (single) with a minimum. We just need to find the row and  
+// //subtract index j's value from the second minimum (not including index j in the row). We then add that to x[j].
+//             int finalJ;
+//             for (int row : J) finalJ = row; //weird syntax, but must happen exactly once because k == 1
+//             for (int i = 1; i <= m; i++) {
+//                 int jVal = matrix.get(i, finalJ) + x.get(finalJ);
+//                 bool singleMin = true; //signifies whether the row has a single minimum
+//                 for (int j = 1; singleMin && j <= n; j++) {
+//                     if (j != finalJ && jVal >= (matrix.get(i, j)) + x.get(j)) singleMin = false;       
+//                 }
+//                 if (singleMin) {
+//                     int secondMin = INT_MAX;
+//                     for (int j = 1; j <= n; j++) {
+//                         if (j != finalJ) secondMin = min(secondMin, matrix.get(i, j) + x.get(j));      
+//                     }
+//                     x.add(finalJ, secondMin - jVal); //achieve final equality
+//                     x.printSolution();
+//                     return EXIT_SUCCESS;
+//                 }
+//             }
+//             cout << "Error: no solution found in solution branch" << endl;
+//             return EXIT_FAILURE;
+//         }
+
+//         //get attracted rows so far
+//         unordered_set<int> attractedRows;
+//         for (int i = 1; i <= m; i++) {
+//             unordered_set<int> minCols;
+//             int rowMinElem = INT_MAX;
+//             for (int j = 1; j <= n; j++) {
+//                 int elem = matrix.get(i, j) + x.get(j);
+//                 if (elem < rowMinElem) {
+//                     minCols = {j};
+//                     rowMinElem = elem;
+//                 }
+//                 else if (elem == rowMinElem) {
+//                     minCols.insert(j);
+//                 }
+//             }
+//             bool valid = true;
+//             for (int minCol : minCols) { //this is inefficient, but it's okay for now
+//                 if (J.find(minCol) == J.end()) {
+//                     valid = false;
+//                     break;
+//                 }
+//             }
+//             if (valid) attractedRows.insert(i);
+//         }
+//         //cout << "aRows size: " << attractedRows.size() << endl;
+
+//         //update x as necessary before continuing
+//         int a = INT_MAX, leftMin = INT_MAX, rightMin = INT_MAX;
+//         for (int i : attractedRows) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (J.find(j) == J.end()) { //cols not in J, for leftMin calculation
+//                     leftMin = min(leftMin, matrix.get(i, j) + x.get(j));
+//                 }
+//                 rightMin = min(rightMin, matrix.get(i, j) + x.get(j));
+//             }
+//             assert(leftMin - rightMin >= 1); //according to paper this must be true
+//             a = min(a, leftMin - rightMin);
+//         }
+//         for (int j : J) x.add(j, a); //add 'a' to x at indices in j
+//     }
+
+    return 0;
+}
+
+void solve_Grigoriev(Matrix &matrix, SolnVector &x) {
+    cout << "before: ";
+    x.printSolution();
+    cout << endl;
     int m = matrix.rows(), n = matrix.cols();
-    SolnVector x(n); //solution vector initialized with zeroes
-
     while (true) {
         unordered_set<int> J;
         for (int i = 1; i <= m; i++) {
@@ -125,11 +258,10 @@ int main(int argc, char *argv[]) {
         int k = J.size();
         if (k == n) {
             cout << "there is no solution for this input" << endl;
-            return EXIT_SUCCESS;
+            return;
         }
-        if (k == 0) { //is this an end case?
-            x.printSolution();
-            return EXIT_SUCCESS;
+        if (k == 0) { //solved already
+            return;
         }
         if (k == 1) {
 
@@ -153,16 +285,18 @@ int main(int argc, char *argv[]) {
                 }
                 if (singleMin) {
                     int secondMin = INT_MAX;
-                    for (int j = 1; j < n; j++) {
+                    for (int j = 1; j <= n; j++) {
                         if (j != finalJ) secondMin = min(secondMin, matrix.get(i, j) + x.get(j));      
                     }
-                    x.add(finalJ, jVal - secondMin); //achieve final equality
+                    x.add(finalJ, secondMin - jVal); //achieve final equality
+                    cout << "after: ";
                     x.printSolution();
-                    return EXIT_SUCCESS;
+                    cout << endl;
+                    return;
                 }
             }
             cout << "Error: no solution found in solution branch" << endl;
-            return EXIT_FAILURE;
+            return;
         }
 
         //get attracted rows so far
@@ -206,18 +340,29 @@ int main(int argc, char *argv[]) {
         for (int j : J) x.add(j, a); //add 'a' to x at indices in j
     }
 
-    return 0;
+}
+
+SolnVector Grigoriev_Algorithm(vector<vector<int>> &matrix) {
+    SolnVector s(matrix[0].size());
+    vector<vector<int>> emptyMatrix = {};
+    Matrix m(emptyMatrix);
+
+    for (vector<int> r : matrix) {
+        m.addRow(r);
+        cout << "before0, with cursize " << m.rows() << ": ";
+        s.printSolution();
+        cout << endl;
+        solve_Grigoriev(m, s);
+        cout << "after0: ";
+        s.printSolution();
+        cout << endl;
+        // s.printSolution();
+    }
+    // printSolution(s);
+    return s;
 }
 
 
-void solve(Matrix &matrix, SolnVector sv) {
-    
-    //for i : {1 .. numRows}
-    //solve i x n submatrix, terminate if impossible
-    //return final sv
-
-    return;
-}
 
 
 
